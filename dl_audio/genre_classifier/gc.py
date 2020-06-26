8c0dbfa2edf4b12ca072464d631ba636848e2bd5
jhyukjung21 <jhyukjung21@kis.ac>
2020-06-26 18:31:55 +0900

    Solved Overfitting

--- dl_audio/genre_classifier/gc.py
+++ dl_audio/genre_classifier/gc.py
@@ -3,6 +3,7 @@ import json
 import numpy as np
 from sklearn.model_selection import train_test_split
 import tensorflow.keras as keras
+import matplotlib.pyplot as plt
 
 DATASET_PATH = "data_10.json"
 
@@ -14,7 +15,30 @@ def load_data(dataset_path):
     inputs = np.array(data["mfcc"])
     targets = np.array(data["labels"])
 
+    print("Data Successfully Loaded")
+
     return inputs, targets
+
+def plot_history(history):
+    fig, axs = plt.subplots(2)
+
+    # create accuracy subplot
+    axs[0].plot(history.history["accuracy"], label="train accuracy")
+    axs[0].plot(history.history["val_accuracy"], label="test accuracy")
+    axs[0].set_ylabel("Accuracy")
+    axs[0].legend(loc="lower right")
+    axs[0].set_title("Accuracy eval")
+
+    # create error subplot
+    axs[1].plot(history.history["loss"], label="train error")
+    axs[1].plot(history.history["val_loss"], label="test error")
+    axs[1].set_ylabel("Error")
+    axs[1].set_xlabel("Epochs")
+    axs[1].legend(loc="upper right")
+    axs[1].set_title("Error eval")
+
+    plt.show()
+
 
 if __name__ == "__main__":
     
@@ -30,13 +54,16 @@ if __name__ == "__main__":
         keras.layers.Flatten(input_shape=(inputs.shape[1], inputs.shape[2])),
 
         # 1st hidden layer
-        keras.layers.Dense(512, activation="relu"),
+        keras.layers.Dense(512, activation="relu", kernel_regularizer=keras.regularizers.l2(0.001)),
+        keras.layers.Dropout(0.3),
 
         # 2nd hidden layer
-        keras.layers.Dense(256, activation="relu"),
+        keras.layers.Dense(256, activation="relu", kernel_regularizer=keras.regularizers.l2(0.001)),
+        keras.layers.Dropout(0.3),
 
         # 3rd hidden layer
-        keras.layers.Dense(64, activation="relu"),
+        keras.layers.Dense(64, activation="relu", kernel_regularizer=keras.regularizers.l2(0.001)),
+        keras.layers.Dropout(0.3),
 
         # output layer
         keras.layers.Dense(10, activation="softmax")
@@ -51,9 +78,10 @@ if __name__ == "__main__":
     model.summary()
 
     # train network
-    model.fit(inputs_train, targets_train, 
-              validation_data=(inputs_test, targets_test),
-              epochs=50,
-              batch_size=32)
+    history = model.fit(inputs_train, targets_train, 
+                        validation_data=(inputs_test, targets_test),
+                        epochs=50,
+                        batch_size=32) 
 
-# %%
+    # plot accuracy and error over the epochs
+    plot_history(history)
