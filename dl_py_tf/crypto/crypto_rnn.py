e2846d826ea2c9fb6e73ad70ac01a0f65db6ffbe
jhyukjung21 <jhyukjung21@kis.ac>
2020-06-27 12:29:46 +0900

    Created, Normalized, Balanced Sequence Data

--- dl_py_tf/crypto/crypto_rnn.py
+++ dl_py_tf/crypto/crypto_rnn.py
@@ -2,6 +2,10 @@
 
 import pandas as pd
 import os
+import numpy as np
+import random
+from sklearn import preprocessing
+from collections import deque
 
 SEQ_LENGTH = 60
 FUTURE_PERIOD_PREDICT = 3
@@ -12,6 +16,59 @@ def classify(current, future):
         return 1
     else:
         return 0
+
+def preprocess_df(df):   
+    df = df.drop("future", 1)
+    
+    for col in df.columns:
+        if col != "target":
+            df[col] = df[col].pct_change()
+            df.dropna(inplace=True)
+            df[col] = preprocessing.scale(df[col].values)
+
+    df.dropna(inplace=True)
+
+    sequential_data = []
+    prev_days = deque(maxlen=SEQ_LENGTH)
+
+    for i in df.values:
+        prev_days.append([n for n in i[:-1]])
+        if len(prev_days) == SEQ_LENGTH:
+            sequential_data.append([np.array(prev_days), i[-1]])
+    
+    random.shuffle(sequential_data)
+
+    buys = []
+    sells = []
+
+    for seq, target in sequential_data:
+        if target == 0:
+            sells.append([seq, target])
+        if target == 1:
+            buys.append([seq, target])
+    
+    random.shuffle(buys)
+    random.shuffle(sells)
+
+    lower = min(len(buys), len(sells))
+
+    # if len in 30k
+    buys = buys[:lower]
+    sells = sells[:lower]
+
+    sequential_data = buys+sells
+    random.shuffle(sequential_data)
+
+    x = []
+    y = []
+
+    for seq, target in sequential_data:
+        x.append(seq)
+        y.append(target)
+        
+    return np.array(x), y
+
+
 
 main_df = pd.DataFrame()
 
@@ -32,5 +89,18 @@ for ratio in ratios:
 
 main_df["future"] = main_df["{}_close".format(RATIO_TO_PREDICT)].shift(-FUTURE_PERIOD_PREDICT)
 main_df["target"] = list(map(classify, main_df["{}_close".format(RATIO_TO_PREDICT)], main_df["future"]))
+# print(main_df[["{}_close".format(RATIO_TO_PREDICT), "future", "target"]].head(10))
 
-print(main_df[["{}_close".format(RATIO_TO_PREDICT), "future", "target"]].head(10))
+times = sorted(main_df.index.values)
+last_5pct = times[-int(0.05*len(times))]
+# print(last_5pct)
+
+validation_main_df = main_df[(main_df.index >= last_5pct)]
+main_df = main_df[(main_df.index < last_5pct)]
+
+x_train, y_train = preprocess_df(main_df)
+x_validation, y_validation = preprocess_df(validation_main_df)
+
+print("train data: {}, validation: {}".format(len(x_train), len(x_validation)))
+print("Dont buys: {}, buys: {}".format(y_train.count(0), y_train.count(1)))
+print("VALIDATION Dont buys: {}, buys: {}".format(y_validation.count(0), y_validation.count(1)))
